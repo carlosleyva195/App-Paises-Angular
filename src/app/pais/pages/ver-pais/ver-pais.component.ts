618ec5e120cc9ece460d109fc4c33584cf6d2c6c
Carlos Leyva <carlosleyva195@gmail.com>
2021-07-20 08:19:02 -0700

    FIRST COMMIT

---
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
+
+@Component({
+  selector: 'app-ver-pais',
+  templateUrl: './ver-pais.component.html',
+  styles: [
+  ]
+})
+export class VerPaisComponent implements OnInit {
+
+  pais!:Country;
+
+  constructor( 
+    private activateRoute: ActivatedRoute,
+    private PaisService: PaisService
+    
+    ) { }
+
+  ngOnInit(): void {
+
+      this.activateRoute.params
+      .pipe(
+        switchMap( ( { id } ) => this.PaisService.getPaisPorAlpha( id ) ),
+        tap( console.log )
+      )
+      .subscribe( pais => this.pais = pais);
+
+    // this.activateRoute.params
+    //   .subscribe( ({ id }) =>{
+    //     console.log(id);
+
+    //     this.PaisService.getPaisPorAlpha( id )
+    //       .subscribe( pais =>{
+    //         console.log(pais);
+    //       });
+    //   });
+  }
+}
